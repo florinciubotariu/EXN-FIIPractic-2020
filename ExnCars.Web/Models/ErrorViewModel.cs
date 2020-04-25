94063cdba53c9fe2b9334f3374ca8d612f5ce0c6
florin.ciubotariu <ciubotariu.florin@gmail.com>
2020-04-25 12:37:22 +0300

    25.04.2020

---
+++ ExnCars.Web/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace ExnCars.Web.Models
+{
+  public class ErrorViewModel
+  {
+    public string RequestId { get; set; }
+
+    public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+  }
+}
